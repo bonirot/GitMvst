1eb7bc854b23c42fcbc9b7b0e92a0a09cb87fb6e
Julia Sanchis <jursanchis@gmail.com>
2024-06-13 13:40:56 +0200

    feat(repository interface

--- src/utils/fetch.functions.ts
+++ src/utils/fetch.functions.ts
@@ -6,9 +6,9 @@ console.log(VITE_BASE_URL);
 export async function fetchUsers(): Promise<IUser[]> {
   try {
     const response = await fetch(`${VITE_BASE_URL}/users`);
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
+    // if (!response.ok) {
+    //   throw new Error("Network response was not ok");
+    // }
     const users: IUser[] = await response.json();
     return users;
   } catch (error) {
