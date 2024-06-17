ead61f9163699b76c3ec50c328b6e16e7cdcd22b
Julia Sanchis <jursanchis@gmail.com>
2024-06-17 18:53:11 +0200

    feat: dotenv

--- src/utils/fetch.functions.ts
+++ src/utils/fetch.functions.ts
@@ -1,14 +1,11 @@
 import { IProfile } from "../interfaces/profile.interface";
 import { IRepository } from "../interfaces/repo.interface";
 
-const url = "https://api.github.com";
+const { VITE_BASE_URL } = import.meta.env;
 
 export async function fetchUsers() {
   try {
-    const response = await fetch(`${url}/users`);
-    // if (!response.ok) {
-    //   throw new Error("Network response was not ok");
-    // }
+    const response = await fetch(`${VITE_BASE_URL}/users`);
     const users = await response.json();
     return users;
   } catch (error) {
@@ -19,7 +16,7 @@ export async function fetchUsers() {
 
 export async function fetchUserProfile(username: string) {
   try {
-    const response = await fetch(`${url}/users/${username}`);
+    const response = await fetch(`${VITE_BASE_URL}/users/${username}`);
     const profile: IProfile = await response.json();
     return profile;
   } catch (error) {
@@ -30,7 +27,7 @@ export async function fetchUserProfile(username: string) {
 
 export async function fetchRepos(username: string | undefined) {
   try {
-    const response = await fetch(`${url}/users/${username}/repos`);
+    const response = await fetch(`${VITE_BASE_URL}/users/${username}/repos`);
     const repos: IRepository[] = await response.json();
     return repos;
   } catch (error) {
